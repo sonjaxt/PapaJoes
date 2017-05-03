0a8637e8fe77e370c9e2ed572a4120b595768501
megsputra <megaputra@10.42.0.225>
2017-05-03 13:56:07 -0700

    this now works

--- src/PapaJoes.cpp
+++ src/PapaJoes.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
 //	List<Product> catalog;
-	cout << "This Work?" << endl; // prints !!!Hello Team!!!
+	cout << "This thing Works!!!" << endl; // prints !!!Hello Team!!!
 	return 0;
 }
 
